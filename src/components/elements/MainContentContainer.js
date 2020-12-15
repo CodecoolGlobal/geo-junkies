b74b9a7cd1e3d37769540823c1401f3fbf8b9401
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-15 23:55:49 +0100

    Change main content width to max

--- src/components/elements/MainContentContainer.js
+++ src/components/elements/MainContentContainer.js
@@ -4,9 +4,8 @@ const Content = styled.div`
   position: fixed;
   top: 0;
   right: 0;
-  width: 90%;
+  width: 100%;
   height: 95%;
-  background-color: lightsalmon;
 `;
 
 export default Content;
