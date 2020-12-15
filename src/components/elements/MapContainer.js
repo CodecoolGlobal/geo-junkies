4476aae3691d1eb27fa52152159c6a2bf96cb38a
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-15 23:54:40 +0100

    Add map container style

---
+++ src/components/elements/MapContainer.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const MapContainer = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 90%;
+  height: 95%;
+`;
+
+export default MapContainer;
