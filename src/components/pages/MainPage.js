5a633e0ca8b396d770ea8f454fefb43e8027ea57
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-15 23:56:29 +0100

    Add style elements to home page

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,10 +1,36 @@
 import React, { useContext } from "react";
 import GetCities from "../../hooks/fetchCities";
 import { CityContext } from "../../contexts/CityContext";
+import styled from "styled-components";
+import "../../App.css";
+
+const MainPageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+// const PlayButton = styled.button`
+//   margin: 37% 40%;
+//   height: 50px;
+//   width: 120px;
+//   border-radius: 20px;
+//   outline: none;
+//   font-weight: bold;
+// `;
 
 export default function MainPage() {
   GetCities();
   const cities = useContext(CityContext)[0];
 
-  return <div></div>;
+  return (
+    <MainPageContainer id="test">
+      <h1>Geo Guesser</h1>
+      <h3>by</h3>
+      <h2>Geo Junkies</h2>
+      <div id="globe"></div>
+    </MainPageContainer>
+  );
 }
