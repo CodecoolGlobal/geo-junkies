c663d58a55e6976e89817a4e683c4791404c99cb
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-15 23:53:38 +0100

    Add navbar style

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,40 @@
 import React from "react";
 import NavbarContainer from "../elements/NavbarContainer";
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
 
-export default function Navbar() {
-  return <NavbarContainer>Navbar:</NavbarContainer>;
+const NavbarLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+const NavbarLink = styled(Link)`
+  color: grey;
+  text-decoration: none;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  padding: 5px;
+  font-weight: bold;
+  font-size: 1.5em;
+  border-radius: 10px;
+  visibility: ${(props) =>
+    useLocation().pathname === props.to ? "hidden" : ""};
+  &:hover {
+    color: black;
+  }
+`;
+
+export default function Navbar(props) {
+  return (
+    <NavbarContainer>
+      <NavbarLinks>
+        <React.Fragment>
+          <NavbarLink to="/">Home</NavbarLink>
+          <NavbarLink to="/map">Play Game</NavbarLink>
+          <NavbarLink to="/scores">High Scores</NavbarLink>
+          <NavbarLink to="/about">About</NavbarLink>
+        </React.Fragment>
+      </NavbarLinks>
+    </NavbarContainer>
+  );
 }
