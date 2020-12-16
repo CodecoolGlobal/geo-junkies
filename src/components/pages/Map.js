adc3e2f30673d147a63ebbe58425a1198764a410
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-16 11:48:26 +0100

    Add username input

--- src/components/pages/Map.js
+++ src/components/pages/Map.js
@@ -3,6 +3,42 @@ import mapboxgl, { LngLat } from "mapbox-gl";
 import MapContainer from "../elements/MapContainer";
 import "../../style/marker.css";
 import data from "../files/europeanCities.json";
+import styled from "styled-components";
+
+const SetUsernameButton = styled.button`
+  border-radius: 10px;
+  width: 80px;
+  padding: 5px;
+  font-size: 1em;
+  font-weight: bold;
+  outline: none;
+  margin-top: 10px;
+`;
+
+const UsernameLabel = styled.label`
+  font-size: 1em;
+`;
+
+const UsernameInput = styled.input`
+  font-size: 1em;
+  margin-top: 5px;
+  border-radius: 5px;
+  padding-left: 3px;
+  outline: none;
+`;
+
+const UsernameContainer = styled.div`
+  position: fixed;
+  top: 40%;
+  left: 45%;
+  padding: 20px;
+  border-radius: 20px;
+  text-align: center;
+  font-weight: bold;
+  background-color: lightgrey;
+  opacity: 0.9;
+  border: 1px solid black;
+`;
 
 const styles = {
   width: "80vw",
@@ -15,58 +51,91 @@ let isPointSelected = false;
 const Map = (props) => {
   const [map, setMap] = useState(null);
   const mapContainer = useRef(null);
+  const [username, setUsername] = useState(null);
 
   let selectedCities = [];
-    while (selectedCities.length < 5) {
-      let cityIndex = Math.floor(Math.random() * data.european_cities.length);
-      let actualCity = data.european_cities[cityIndex];
-      if (!selectedCities.includes(actualCity)) {
-        selectedCities.push(actualCity);
-      }
+  while (selectedCities.length < 5) {
+    let cityIndex = Math.floor(Math.random() * data.european_cities.length);
+    let actualCity = data.european_cities[cityIndex];
+    if (!selectedCities.includes(actualCity)) {
+      selectedCities.push(actualCity);
     }
+  }
 
   const cityArray = selectedCities;
 
   const [currentCity, setCurrentCity] = useState(cityArray[0]);
 
   useEffect(() => {
-    mapboxgl.accessToken =
-      "pk.eyJ1Ijoia296bWFydGludXMiLCJhIjoiY2tpb2VwNW91MGh6bDJ6bWxkbzdlemUyeCJ9.JcDXIp8INuk9kw1H3BAt8Q";
-    const initializeMap = ({ setMap, mapContainer }) => {
-      const map = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: "mapbox://styles/mapbox/streets-v11",
-        center: [25, 55],
-        zoom: 2.75,
-      });
-
-      map.on("load", () => {
-        setMap(map);
-        disableInteractives(map);
-
-        map.on("click", (e) => {
-          if (isPointSelected) {
-            e.preventDefault();
-          } else {
-            mapClickHandler(e, map, currentCity);
-          }
+    if (username) {
+      mapboxgl.accessToken =
+        "pk.eyJ1Ijoia296bWFydGludXMiLCJhIjoiY2tpb2VwNW91MGh6bDJ6bWxkbzdlemUyeCJ9.JcDXIp8INuk9kw1H3BAt8Q";
+      const initializeMap = ({ setMap, mapContainer }) => {
+        const map = new mapboxgl.Map({
+          container: mapContainer.current,
+          style: "mapbox://styles/mapbox/streets-v11",
+          center: [25, 55],
+          zoom: 2.75,
         });
-        map.resize();
-      });
-    };
 
-    if (!map) initializeMap({ setMap, mapContainer });
-  }, [map]);
+        map.on("load", () => {
+          setMap(map);
+          disableInteractives(map);
 
-  return (
-    <MapContainer>
-      <p>{currentCity.city}</p>
-      <div ref={(el) => (mapContainer.current = el)} style={styles} />
-      <div>
-        <button id="clearButton">Evaluate</button>
-      </div>
-    </MapContainer>
-  );
+          map.on("click", (e) => {
+            if (isPointSelected) {
+              e.preventDefault();
+            } else {
+              mapClickHandler(e, map, currentCity);
+            }
+          });
+          map.resize();
+        });
+      };
+
+      if (!map) initializeMap({ setMap, mapContainer });
+    }
+  }, [map, username]);
+
+  if (username) {
+    return (
+      <MapContainer>
+        <p>{currentCity.city}</p>
+        <p>Current user: {username}</p>
+        <div ref={(el) => (mapContainer.current = el)} style={styles} />
+        <div>
+          <button id="clearButton">Evaluate</button>
+        </div>
+      </MapContainer>
+    );
+  } else {
+    return (
+      <MapContainer>
+        <UsernameContainer id="usernameContainer">
+          <p>
+            <UsernameLabel>Please enter your name:</UsernameLabel>
+          </p>
+          <p>
+            <UsernameInput
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Username"
+            />
+          </p>
+          <p>
+            <SetUsernameButton
+              onClick={() => {
+                setUsername(document.querySelector("#username").value);
+              }}
+            >
+              Submit
+            </SetUsernameButton>
+          </p>
+        </UsernameContainer>
+      </MapContainer>
+    );
+  }
 };
 
 function disableInteractives(map) {
