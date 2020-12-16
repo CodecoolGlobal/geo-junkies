0aed5815efe4dd176d1197db57cdcdc17b2a0d60
sandyrnelson <sandyrnelson@berkeley.edu>
2020-12-16 13:52:32 +0100

    Page now loads top ten based on scores

--- src/components/pages/HighScores.js
+++ src/components/pages/HighScores.js
@@ -1,49 +1,57 @@
 import React, { useContext } from "react";
 import styled from "styled-components";
 import { HighScoreContext } from "../../contexts/HighScoreContext";
+import "../../style/high-score.css";
 
 const PlayersContainer = styled.div`
 	margin-top: 5%;
 	margin-left: 25%;
 	width: 60%;
-	/* border: 5px solid #ffff00; */
+	border: 5px solid lightgrey;
 	padding: 10px;
-	/* background-color: black; */
-	display: flex;
-	justify-content: space-evenly;
-`;
-
-const PlayersCard = styled.div`
-	height: 200px;
-	width: 200px;
-	padding: 5%;
 	background-color: #fff;
-	align-items: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
 `;
 
 export default function HighScores() {
 	const players = useContext(HighScoreContext)[0];
+	const setPlayers = useContext(HighScoreContext)[1];
 
-	const getTop5Scores = (scoresArray) => {
-		scoresArray.sort(function (a, b) {
-			return a - b;
+	const getTop10Scores = () => {
+		players.sort(function (a, b) {
+			return a.score - b.score;
 		});
-		return scoresArray.reverse().slice(0, 5);
+		return players.reverse().slice(0, 10);
 	};
 
-	let content = <p>Loading...</p>;
+	// setPlayers((prev) => [...prev, { name: "Marci", score: 9000 }]);
+	let content = <p>No Scores Saved</p>;
 
 	if (players && players.length > 0) {
+		const orderedPlayers = getTop10Scores();
 		content = (
 			<PlayersContainer>
-				{players.map((player) => (
-					<PlayersCard>
-						<div>{player.name}</div>
-						{getTop5Scores(player.scores).map((score) => (
-							<div>{score}</div>
+				<h1 className="score-title">High Scores</h1>
+				<table>
+					<thead>
+						<tr>
+							<th className="rank">Rank</th>
+							<th className="players">Players</th>
+							<th className="scores">Scores</th>
+						</tr>
+					</thead>
+					<tbody>
+						{orderedPlayers.map((player, index) => (
+							<tr>
+								<td>{index + 1}</td>
+								<td>{player.name}</td>
+								<td>{player.score}</td>
+							</tr>
 						))}
-					</PlayersCard>
-				))}
+					</tbody>
+				</table>
 			</PlayersContainer>
 		);
 	}
