b5cb6b02e8535a30b136d534464c2ba7ca548bd5
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-16 15:17:55 +0100

    style: add about page styling

---
+++ src/components/elements/AboutPageContainer.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const AboutPageContainer = styled.div`
+  position: fixed;
+  top: 10%;
+  right: 20%;
+  min-width: 50%;
+  min-height: 65%;
+  background-color: lightgrey;
+  opacity: 0.9;
+  border-radius: 30px;
+  text-align: right;
+`;
+
+export default AboutPageContainer;
