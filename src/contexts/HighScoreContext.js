a3ee84db535773173754239983a8c49e4182fc2e
sandyrnelson <sandyrnelson@berkeley.edu>
2020-12-16 11:43:43 +0100

    Add test object into context

--- src/contexts/HighScoreContext.js
+++ src/contexts/HighScoreContext.js
@@ -3,11 +3,19 @@ import React, { useState, createContext } from "react";
 export const HighScoreContext = createContext();
 
 export const HighScoreContextProvider = (props) => {
-  const [highScore, setHighScore] = useState([]);
+	const [highScore, setHighScore] = useState([
+		{
+			name: "Sandy",
+      scores: [2500, 4900, 3000]},
+      {
+      name: "Oli",
+			scores: [2140, 600, 330000],
+		},
+	]);
 
-  return (
-    <HighScoreContext.Provider value={[highScore, setHighScore]}>
-      {props.children}
-    </HighScoreContext.Provider>
-  );
+	return (
+		<HighScoreContext.Provider value={[highScore, setHighScore]}>
+			{props.children}
+		</HighScoreContext.Provider>
+	);
 };
