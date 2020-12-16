86260d1b2e41972f468f9033e26c798082230786
Oliver Simon <oliversimon104@gmail.com>
2020-12-16 15:14:11 +0100

    refactor: change marker positioning

--- src/components/pages/Map.js
+++ src/components/pages/Map.js
@@ -3,7 +3,7 @@ import mapboxgl, { LngLat } from "mapbox-gl";
 import MapContainer from "../elements/MapContainer";
 import "../../style/marker.css";
 import data from "../files/europeanCities.json";
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, Marker } from "react-mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const MapBox = ReactMapboxGl({
@@ -27,18 +27,25 @@ const MapBox = ReactMapboxGl({
 let isPointSelected = false;
 const roundNumber = 5;
 let currentRound = 0;
-let selectedCities = [];
-let currentCity = null;
+// let selectedCities = [];
+// let currentCity = null;
 
 const Map = (props) => {
-  const [map, setMap] = useState(null);
-  const mapContainer = useRef(null);
+  // const [map, setMap] = useState(null);
+  // const mapContainer = useRef(null);
+  // currentCity = selectedCities[currentRound];
+  const [selectedCities, setSelectedCities] = useState(citySelector());
+  const [currentCity, setCurrentCity] = useState(selectedCities[currentRound]);
+  const [markerLng, setMarkerLng] = useState(currentCity.longitude);
+  const [markerLat, setMarkerLat] = useState(currentCity.latitude);
+  // citySelector(setSelectedCities);
 
-  citySelector();
-
+  // useEffect(() => {
+  //   setCurrentCity(selectedCities[0]);
+  //   setMarkerLng(currentCity.longitude);
+  //   setMarkerLat(currentCity.latitude);
+  // }, []);
   // const cityArray = selectedCities;
-
-  currentCity = selectedCities[currentRound];
 
   // useEffect(() => {
   //   // mapboxgl.accessToken =
@@ -71,34 +78,46 @@ const Map = (props) => {
   //   if (!map) initializeMap({ setMap, mapContainer });
   // }, [map, currentCity]);
 
-  return (
-    <MapContainer>
-      <p>{currentCity.city}</p>
-      <MapBox
-        style="mapbox://styles/mapbox/streets-v11"
-        containerStyle={{
-          height: "680px",
-          width: "800px",
-        }}
-        center={[15, 57]}
-        zoom={[2.75]}
-        onStyleLoad={(map, event) =>
-          map.style.stylesheet.layers.forEach(function (layer) {
-            if (layer.type === "symbol") {
-              map.removeLayer(layer.id);
-            }
-          })
-        }
-      ></MapBox>
-      ;
-      <div>
-        <button id="clearButton">Next City</button>
-      </div>
-    </MapContainer>
-  );
+  let content = "LOADING";
+  if (currentCity) {
+    content = (
+      <MapContainer>
+        <p>{currentCity.city}</p>
+        <MapBox
+          style="mapbox://styles/mapbox/streets-v11"
+          containerStyle={{
+            height: "680px",
+            width: "800px",
+          }}
+          center={[15, 57]}
+          zoom={[2.75]}
+          onStyleLoad={(map, event) =>
+            map.style.stylesheet.layers.forEach(function (layer) {
+              if (layer.type === "symbol") {
+                map.removeLayer(layer.id);
+              }
+            })
+          }
+        >
+          <Marker coordinates={[markerLng, markerLat]} offset={-6}>
+            <img
+              src="https://img.icons8.com/color/48/000000/marker.png"
+              alt=""
+            />
+          </Marker>
+        </MapBox>
+        <div>
+          <button id="clearButton">Next City</button>
+        </div>
+      </MapContainer>
+    );
+  }
+
+  return content;
 };
 
 function citySelector() {
+  let selectedCities = [];
   while (selectedCities.length < roundNumber) {
     let cityIndex = Math.floor(Math.random() * data.european_cities.length);
     let actualCity = data.european_cities[cityIndex];
@@ -106,43 +125,44 @@ function citySelector() {
       selectedCities.push(actualCity);
     }
   }
+  return selectedCities;
 }
 
-function disableInteractives(map) {
-  map.scrollZoom.disable();
-  map.doubleClickZoom.disable();
-  map.dragPan.disable();
-  MapBox.style.stylesheet.layers.forEach(function (layer) {
-    if (layer.type === "symbol") {
-      map.removeLayer(layer.id);
-    }
-  });
-}
+// function disableInteractives(map) {
+//   map.scrollZoom.disable();
+//   map.doubleClickZoom.disable();
+//   map.dragPan.disable();
+//   MapBox.style.stylesheet.layers.forEach(function (layer) {
+//     if (layer.type === "symbol") {
+//       map.removeLayer(layer.id);
+//     }
+//   });
+// }
 
 const mapClickHandler = (e, map, currentCity) => {
   const city = new LngLat(currentCity.longitude, currentCity.latitude);
   let message = Math.round(city.distanceTo(e.lngLat) / 1000) + " km away.";
-  let guessMarker = new mapboxgl.Marker()
+  let guessMarker = new ReactMapboxGl.Marker()
     .setLngLat([e.lngLat.lng, e.lngLat.lat])
     .addTo(map);
-  let popup = new mapboxgl.Popup({ offset: 38 })
+  let popup = new ReactMapboxGl.Popup({ offset: 38 })
     .setLngLat(city)
     .setHTML(`<h3 class="popup">${message}</h3>`)
     .addTo(map);
-  let cityMarker = new mapboxgl.Marker({ color: "green" })
+  let cityMarker = new ReactMapboxGl.Marker({ color: "green" })
     .setLngLat(city)
     .addTo(map);
 
   isPointSelected = true;
 
-  document.querySelector("#clearButton").addEventListener("click", function () {
-    currentRound++;
-    currentCity = selectedCities[currentRound];
-    guessMarker.remove();
-    cityMarker.remove();
-    popup.remove();
-    isPointSelected = false;
-  });
+  // document.querySelector("#clearButton").addEventListener("click", function () {
+  //   currentRound++;
+  //   currentCity = selectedCities[currentRound];
+  //   guessMarker.remove();
+  //   cityMarker.remove();
+  //   popup.remove();
+  //   isPointSelected = false;
+  // });
 };
 
 export default Map;
