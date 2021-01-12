d06e168359da5443998b6cc334df02b9e453cd8b
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-12 16:10:01 +0100

    feature: logout page

---
+++ src/components/pages/LogoutPage.js
@@ -0,0 +1,27 @@
+import { useEffect, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import UseDeleteData from "../../hooks/UseDelete";
+import { UserContext } from "../../contexts/UserContext";
+import APIs from "../files/ApiRequestURL.json";
+
+const LogoutPage = (props) => {
+  const [user, setUser] = useContext(UserContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    UseDeleteData(APIs.logout, user.token, (response) => {
+      if (response.status === 204) {
+        setUser({
+          username: false,
+          token: false,
+        });
+        return history.push("/");
+      }
+    });
+    return history.goBack("");
+  }, []);
+
+  return "";
+};
+
+export default LogoutPage;
