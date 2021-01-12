65cc7d1ff631a7abb70f0a06994a8d89a0dfdfb6
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-12 16:10:55 +0100

    feature: change login-logout button

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import NavbarContainer from "../elements/NavbarContainer";
+import { UserContext } from "../../contexts/UserContext";
 import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
 
@@ -25,13 +26,37 @@ const NavbarLink = styled(Link)`
   }
 `;
 
+const RegNavbarLink = styled(Link)`
+  color: grey;
+  text-decoration: none;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  padding: 5px;
+  font-weight: bold;
+  font-size: 1.5em;
+  letter-spacing: 0.025em;
+  border-radius: 10px;
+  visibility: ${(props) => (props.user.token ? "hidden" : "")};
+  &:hover {
+    color: black;
+  }
+`;
+
 export default function Navbar(props) {
+  const user = useContext(UserContext)[0];
+
   return (
     <NavbarContainer>
       <NavbarLinks>
         <React.Fragment>
           <NavbarLink to="/">Home</NavbarLink>
-          <NavbarLink to="/login">Login</NavbarLink>
+          <RegNavbarLink user={user} to="/registration">
+            Registration
+          </RegNavbarLink>
+          {!user.token ? (
+            <NavbarLink to="/login">Login</NavbarLink>
+          ) : (
+            <NavbarLink to="/logout">Logout</NavbarLink>
+          )}
           <NavbarLink to="/map">Play Game</NavbarLink>
           <NavbarLink to="/scores">High Scores</NavbarLink>
           <NavbarLink to="/about">About</NavbarLink>
