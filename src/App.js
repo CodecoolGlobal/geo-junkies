90d1ceca6c3593156cb5d2f49094ca83c16003d4
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-12 14:37:54 +0100

    feature: add user context

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import MainComponent from "./components/layout/MainComponent";
 import { CityContextProvider } from "./contexts/CityContext";
+import { UserContextProvider } from "./contexts/UserContext";
 import { HighScoreContextProvider } from "./contexts/HighScoreContext";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./style/App.css";
 
 function App() {
   return (
-    <CityContextProvider>
-      <HighScoreContextProvider>
-        <Router>
-          <MainComponent />
-        </Router>
-      </HighScoreContextProvider>
-    </CityContextProvider>
+    <UserContextProvider>
+      <CityContextProvider>
+        <HighScoreContextProvider>
+          <Router>
+            <MainComponent />
+          </Router>
+        </HighScoreContextProvider>
+      </CityContextProvider>
+    </UserContextProvider>
   );
 }
 
