762c492adb31ff773a056b1e945a98cd8572494a
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-13 09:21:38 +0100

    style: responsive navbar

--- src/components/elements/NavbarContainer.js
+++ src/components/elements/NavbarContainer.js
@@ -13,6 +13,25 @@ const NavbarContainer = styled.div`
   z-index: 9999;
   border-radius: 0 0 20px 0;
   border: 1px solid darkgrey;
+  @media screen and (max-width: 1520px) {
+    font-size: 0.9em;
+  }
+  @media screen and (max-width: 1400px) {
+    font-size: 0.8em;
+  }
+  @media screen and (max-width: 1300px) {
+    font-size: 0.75em;
+  }
+  @media screen and (max-width: 1200px) {
+    font-size: 0.7em;
+  }
+  @media screen and (max-width: 1100px) {
+    font-size: 0.65em;
+  }
+  @media screen and (max-width: 1000px) {
+    font-size: 0.6em;
+    min-width: 100px;
+  }
 `;
 
 export default NavbarContainer;
