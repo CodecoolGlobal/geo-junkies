2e782e7842ef76adbbf0a7f2e342496ad77af26d
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-13 23:31:44 +0100

    feature: stages add actual map context

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import { UserContextProvider } from "./contexts/UserContext";
 import { HighScoreContextProvider } from "./contexts/HighScoreContext";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./style/App.css";
+import { ActualMapContextProvider } from "./contexts/ActualMapContext";
 
 function App() {
   return (
     <UserContextProvider>
       <CityContextProvider>
-        <HighScoreContextProvider>
-          <Router>
-            <MainComponent />
-          </Router>
-        </HighScoreContextProvider>
+        <ActualMapContextProvider>
+          <HighScoreContextProvider>
+            <Router>
+              <MainComponent />
+            </Router>
+          </HighScoreContextProvider>
+        </ActualMapContextProvider>
       </CityContextProvider>
     </UserContextProvider>
   );
