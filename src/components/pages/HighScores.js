18026fb45b5358b6a23ae88ba64b2f206882ae31
sandyrnelson <sandyrnelson@berkeley.edu>
2021-01-14 10:19:07 +0100

    refactor: refator styling source

--- src/components/pages/HighScores.js
+++ src/components/pages/HighScores.js
@@ -3,64 +3,8 @@ import styled from "styled-components";
 import "../../style/high-score.css";
 import APIs from "../files/ApiRequestURL.json";
 import axios from "axios";
+import PlayersContainer from "../elements/PlayerContainer";
 
-const PlayersContainer = styled.div`
-	margin-top: 5%;
-	margin-left: 25%;
-	width: 60%;
-	border: 5px solid lightgrey;
-	padding: 10px;
-	background-color: #fff;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-evenly;
-	letter-spacing: 0.1em;
-
-	.buttonBox {
-		height: 10%;
-		margin-bottom: -3px;
-
-		display: flex;
-		justify-content: space-around;
-		align-items: flex-end;
-
-		button {
-			width: 50%;
-			padding: 6px 0;
-			outline: none;
-			cursor: pointer;
-			border: none;
-			border-bottom: white solid 3px;
-			border-right: white solid 3px;
-			background-color: rgba(163, 163, 163);
-		}
-
-		.active {
-			font-weight: 750;
-			background-color: lightgrey;
-		}
-
-		#leftButton {
-			border-radius: 15px 0 0;
-		}
-
-		#rightButton {
-			border-radius: 0 15px 0 0;
-			border-right: none;
-		}
-
-		.map-title {
-			font-size: 3rem;
-			text-align: center;
-			color: rgb(231, 231, 195);
-			text-decoration: none;
-			text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
-			padding: 5px;
-			font-weight: bold;
-			font-size: 1.5em;
-		}
-	}
-`;
 
 export default function HighScores() {
 	const [players, setPlayers] = useState([]);
