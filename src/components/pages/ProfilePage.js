39588ec67af6128ec5e0cce9e2b27bc63d599f4e
sandyrnelson <sandyrnelson@berkeley.edu>
2021-02-09 13:26:04 +0100

    style: remove unused styled component (replaced with plyercontainer)

--- src/components/pages/ProfilePage.js
+++ src/components/pages/ProfilePage.js
@@ -9,64 +9,6 @@ import PlayersContainer from "../elements/PlayerContainer";
 import "../../style/High-score.css";
 
 
-const ProfileContainer = styled.div`
-	margin-top: 5%;
-	margin-left: 15%;
-	width: 80%;
-	border: 5px solid lightgrey;
-	padding: 10px;
-	background-color: #fff;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-evenly;
-	letter-spacing: 0.1em;
-
-	.buttonBox {
-		height: 10%;
-		margin-bottom: -3px;
-
-		display: flex;
-		justify-content: space-around;
-		align-items: flex-end;
-
-		button {
-			width: 50%;
-			padding: 6px 0;
-			outline: none;
-			cursor: pointer;
-			border: none;
-			border-bottom: white solid 3px;
-			border-right: white solid 3px;
-			background-color: rgba(163, 163, 163);
-		}
-
-		.active {
-			font-weight: 750;
-			background-color: lightgrey;
-		}
-
-		#leftButton {
-			border-radius: 15px 0 0;
-		}
-
-		#rightButton {
-			border-radius: 0 15px 0 0;
-			border-right: none;
-		}
-
-		.map-title {
-			font-size: 3rem;
-			text-align: center;
-			color: rgb(231, 231, 195);
-			text-decoration: none;
-			text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
-			padding: 5px;
-			font-weight: bold;
-			font-size: 1.5em;
-		}
-	}
-`;
-
 export default function ProfilePage() {
 	const user = useContext(UserContext)[0];
 	const [scores, setScores] = useState();
