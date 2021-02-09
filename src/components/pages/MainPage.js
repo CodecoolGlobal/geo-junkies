80440197c8cba52b1a9a5e697eccc2b380d2bedd
Oliver Simon <oliversimon104@gmail.com>
2021-02-09 11:31:12 +0100

    style : remove unused code lines, add game description page

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -5,7 +5,7 @@ import "../../style/App.css";
 const MainPageContainer = styled.div`
   width: 100%;
   height: 100%;
-  display: flex;
+  display: inline-block;
   align-items: center;
   flex-direction: column;
   font-size: 2em;
@@ -14,23 +14,12 @@ const MainPageContainer = styled.div`
   font-family: Arial, sans-serif;
 `;
 
-const Title = styled.h1`
-  color: black;
-  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
-  border-radius: 25px;
-  border: 1px solid #cbcbcb;
-  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.18);
-  background-color: lightgray;
-  padding: 5px 20px 5px 20px;
-  margin-top: 20px;
-`;
-
 export default function MainPage() {
   return (
     <MainPageContainer id="test">
-      <Title>Geo Guesser</Title>
-      {/* <h3>by</h3> */}
-      {/* <h2>Geo Junkies</h2> */}
+      <div id="main-describe">
+        <p>Game Description</p>
+      </div>
       <div id="globe"></div>
     </MainPageContainer>
   );
